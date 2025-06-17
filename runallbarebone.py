726f0028837db7070a44c8bf505c39875b005ad9
dangkhoi86 <48778790+dangkhoi86@users.noreply.github.com>
2025-06-17 09:03:46 +0700

    Update runallbarebone.py

--- runallbarebone.py
+++ runallbarebone.py
@@ -4,11 +4,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 import os
 
-# print("Running barebone5giay.py...")
-# subprocess.run(["python", "barebone5giay.py"], check=True)
+print("Running barebone5giay.py...")
+subprocess.run(["python", "barebone5giay.py"], check=True)
 
-# print("Running barebone5giayvtmk.py...")
-# subprocess.run(["python", "barebone5giayvtmk.py"], check=True)
+print("Running barebone5giayvtmk.py...")
+subprocess.run(["python", "barebone5giayvtmk.py"], check=True)
 
 print("Running barebone5giaymkcom.py...")
 subprocess.run(["python", "barebone5giaymkcom.py"], check=True)
