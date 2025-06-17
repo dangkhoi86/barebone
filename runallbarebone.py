ef070a52ceccd1e016ca4b9ea07377cdd42f0a4d
dangkhoi86 <48778790+dangkhoi86@users.noreply.github.com>
2025-06-17 09:01:04 +0700

    Update runallbarebone.py

--- runallbarebone.py
+++ runallbarebone.py
@@ -7,11 +7,11 @@ import os
 # print("Running barebone5giay.py...")
 # subprocess.run(["python", "barebone5giay.py"], check=True)
 
-print("Running barebone5giayvtmk.py...")
-subprocess.run(["python", "barebone5giayvtmk.py"], check=True)
+# print("Running barebone5giayvtmk.py...")
+# subprocess.run(["python", "barebone5giayvtmk.py"], check=True)
 
-# print("Running barebone5giaymkcom.py...")
-# subprocess.run(["python", "barebone5giaymkcom.py"], check=True)
+print("Running barebone5giaymkcom.py...")
+subprocess.run(["python", "barebone5giaymkcom.py"], check=True)
 
 print("Crawlers finished. Cleaning up old sheets...")
 
